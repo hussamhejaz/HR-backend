a160b850a90a89127c4960f31ea5eaf1a4dabb19
hussamhejaz <ooooo.ooooo.hosam@gmail.com>
2025-07-20 15:48:53 +0300

    Fix firebaseAdmin to use env var in production

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,19 +1,12 @@
-// server/config/firebaseAdmin.js
+// config/firebaseAdmin.js
 require("dotenv").config();
 const admin = require("firebase-admin");
 
 let serviceAccount;
 
 if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-  // In CI / Render: we’ve stored the JSON string in this env var
-  try {
-    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-  } catch (err) {
-    console.error("Invalid FIREBASE_SERVICE_ACCOUNT JSON", err);
-    process.exit(1);
-  }
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 } else {
-  // Local dev: read your file (but this file should be gitignored)
   serviceAccount = require("../serviceAccountKey.json");
 }
 
@@ -22,5 +15,6 @@ admin.initializeApp({
   databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
-const db = admin.database();    // or .firestore()
+const db = admin.database();
 module.exports = { admin, db };
+
